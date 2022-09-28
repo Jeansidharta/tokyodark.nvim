0ffca6be6c6df307a2440b27305c9b59ca2d5436
Jean Sidharta <jeansidharta@gmail.com>
2022-09-28 14:21:40 -0300

    Added more neon colors.

--- lua/tokyodark/palette.lua
+++ lua/tokyodark/palette.lua
@@ -2,39 +2,39 @@ local color_gamma = require("tokyodark.utils").color_gamma
 local gamma = require("tokyodark.config").gamma
 
 local colors = {
-    black = "#06080A",
-    bg0 = "#0D0C1E",
-    bg1 = "#1A1B2A",
-    bg2 = "#212234",
-    bg3 = "#353945",
-    bg4 = "#4A5057",
-    bg5 = "#282c34",
-    bg_red = "#FE6D85",
-    bg_green = "#98C379",
-    bg_blue = "#9FBBF3",
-    diff_red = "#773440",
-    diff_green = "#587738",
-    diff_blue = "#354A77",
-    diff_add = "#1E2326",
-    diff_change = "#262b3d",
-    diff_delete = "#281B27",
-    fg = "#A0A8CD",
-    red = "#EE6D85",
-    orange = "#F6955B",
-    yellow = "#D7A65F",
-    green = "#95C561",
-    blue = "#7199EE",
-    cyan = "#38A89D",
-    purple = "#A485DD",
-    grey = "#4A5057",
-    none = "NONE",
+	black = "#06080A",
+	bg0 = "#0D0C1E",
+	bg1 = "#1b1a33",
+	bg2 = "#14122e",
+	bg3 = "#171536",
+	bg4 = "#494766",
+	bg5 = "#1d1b45",
+	BG_RED = "#B3007A",
+	bg_green = "#98C379",
+	bg_blue = "#9FBBF3",
+	diff_red = "#773440",
+	diff_green = "#587738",
+	diff_blue = "#354A77",
+	diff_add = "#1E2326",
+	diff_change = "#262b3d",
+	diff_delete = "#281B27",
+	fg = "#A0A8CD",
+	red = "#ea00d9",
+	orange = "#e68600",
+	yellow = "#00dee6",
+	green = "#12de00",
+	blue = "#0060e6",
+	cyan = "#D7A65F",
+	purple = "#8c33ff",
+	grey = "#4A5057",
+	none = "NONE",
 }
 local function gamma_correction(colors)
-    local colors_corrected = {}
-    for k, v in pairs(colors) do
-        colors_corrected[k] = color_gamma(v, gamma)
-    end
-    return colors_corrected
+	local colors_corrected = {}
+	for k, v in pairs(colors) do
+		colors_corrected[k] = color_gamma(v, gamma)
+	end
+	return colors_corrected
 end
 
 return gamma_correction(colors)
